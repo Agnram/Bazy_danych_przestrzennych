2233553379cca2a5f548e795de3d7944e335886f
Agnieszka Ramian <agnieszkaramian5@gmail.com>
2024-01-07 23:46:00 +0100

    cw10

--- z4.sql
+++
@@ -1,8 +0,0 @@
-CREATE EXTENSION postgis;
-CREATE EXTENSION postgis_raster;
-
------------------------------------------------------------
-
-SELECT ST_Union(geom) 
-INTO scalone
-FROM public."Exports" ;
